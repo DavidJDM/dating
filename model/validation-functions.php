96abe94ffd9aecb7e35e19a87e0515235021c717
David Kovalevich <kovalevichdavid@yahoo.com>
2019-02-04 18:08:10 -0500

    Added validation

---
+++ model/validation-functions.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: davidkovalevich
+ * Date: 2/4/19
+ * Time: 3:06 PM
+ */
+/*
+ * Validate a color
+ *
+ * @param String color
+ * @return boolean
+ */
+
+function validName()
+{
+}
+
+function validAge()
+{
+}
+
+function validPhone()
+{
+}
+
+function validOutdoor()
+{
+}
+
+function validIndoor()
+{
+}
