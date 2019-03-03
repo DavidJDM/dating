838dfe19c1c24ba1255b982347af079d77a15801
David Kovalevich <kovalevichdavid@yahoo.com>
2019-03-03 02:40:59 -0500

    Added Comments

--- model/database.php
+++ model/database.php
@@ -6,60 +6,56 @@
  * Time: 5:34 PM
  */
 
+//Show Errors
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+//Info for DB conection
 require_once ('/home/dkovalev/config.php');
 
 class Database {
+    //Connects to Database
     function connect()
     {
-        try {
+        try { //Try connecting to database
             $dbh = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
             return $dbh;
         }
-        catch(PDOException $e) {
+        catch(PDOException $e) { //If connection unsuccessful, throw an exception
             echo $e->getMessage();
             return false;
         }
     }
 
+    //Inserts a Member into the database
     function insertMember($fname, $lname, $age, $gender, $phone, $email, $state, $seeking, $bio, $premium, $image, $interests)
     {
         global $dbh;
-        $insert = "INSERT INTO `members`(`fname`, `lname`, `age`, `gender`, `phone`, `email`, `state`, `seeking`, `bio`, `premium`, `image`, `interests`) VALUES ('$fname', '$lname', $age, '$gender', '$phone', '$email', '$state', '$seeking', '$bio', $premium, '$image', '$interests');";
-        $statement = $dbh->prepare($insert);
-        $execute = $statement->execute();
+        $insert = "INSERT INTO `members`(`fname`, `lname`, `age`, `gender`, `phone`, `email`, `state`, `seeking`, `bio`, `premium`, `image`, `interests`) VALUES ('$fname', '$lname', $age, '$gender', '$phone', '$email', '$state', '$seeking', '$bio', $premium, '$image', '$interests');"; //SQL Statement
+        $statement = $dbh->prepare($insert); //Prepare Statement
+        $execute = $statement->execute(); //Execute
         return $execute;
     }
 
+    //Returns all of the members registered on the site
     function getMembers()
     {
         global $dbh;
-
-        $fetchMembers = "SELECT * FROM members ORDER BY lname";
-
-        $statement = $dbh->prepare($fetchMembers);
-
-        $statement->execute();
-
+        $fetchMembers = "SELECT * FROM members ORDER BY lname"; //SQL Statement
+        $statement = $dbh->prepare($fetchMembers); //Prepare Statement
+        $statement->execute(); //Execute
         $result = $statement->fetchAll(PDO::FETCH_ASSOC);
-
         return $result;
     }
 
+    //Gets the specified members information based on ID provided
     function getMember($id)
     {
         global $dbh;
-
-        $getMember = "SELECT * FROM `members` WHERE member_id = '$id';";
-
-        $statement = $dbh->prepare($getMember);
-
-        $statement->execute();
-
+        $getMember = "SELECT * FROM `members` WHERE member_id = '$id';"; //SQL Statement
+        $statement = $dbh->prepare($getMember); //Prepare Statement
+        $statement->execute(); //Execute
         $result = $statement->fetchAll(PDO::FETCH_ASSOC);
-
         return $result;
     }
 }
